89234ea0ed711c6dee70ffcc8056bd18e1ef7ece
Ashok Nalam <dotnetmirror@gmail.com>
2021-01-18 19:12:34 +0530

    Intial code to reverse an array in c#

---
+++ Rev_Array_Using_For_Loop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DotNetMirror
+{
+    class ArrayReverseWithForLoop
+    {
+        static void Main()
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7 }; //user input
+            int arrLen = array.Length;
+            int j = 0;
+            int[] revArray = new int[arrLen]; //take same length of target array to reverse
+
+            for (int i = arrLen - 1; i >= 0; i--)
+            {
+                revArray[j] = array[i];
+                j++;
+            }
+            Console.WriteLine("Result: {0}", string.Join(",", revArray));
+            Console.ReadKey();
+        }
+    }
+}
